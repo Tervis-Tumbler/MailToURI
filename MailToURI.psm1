e42b59f5b57c63c455396678ea145497094e5258
ChrisMagnuson <hourglasssand@gmail.com>
2016-10-08 17:05:18 -0400

    Initial commit

---
+++ MailToURI.psm1
@@ -0,0 +1,13 @@
+﻿function New-MailToURI {
+    param (
+        $To,
+        $Cc,
+        $Subject,
+        $Body
+    )
+    if ($Cc) {
+        "mailto:$($To)?subject=$([Uri]::EscapeDataString($Subject))&cc=$([Uri]::EscapeDataString($Cc))&body=$([Uri]::EscapeDataString($Body))"
+    } else {
+        "mailto:$($To)?subject=$([Uri]::EscapeDataString($Subject))&body=$([Uri]::EscapeDataString($Body))"
+    }
+}
